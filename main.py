49890f1d17a96b135d23f8689416875c199ee293
le.toille <125640909+letoille@users.noreply.github.com>
2025-05-08 10:36:05 +0800

    default port

--- main.py
+++ main.py
@@ -72,4 +72,4 @@ if __name__ == "__main__":
     else:
         logging.info("ChromaDB already loaded, skipping load.")
 
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="127.0.0.1", port=9527)
