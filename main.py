36038edf24eb765b017156e808cb8cbd046acaee
le.toille <125640909+letoille@users.noreply.github.com>
2025-05-08 14:05:53 +0800

    optimize

--- main.py
+++ main.py
@@ -54,18 +54,20 @@ class Question(BaseModel):
 @app.post('/api/xl/question')
 def xl_question(question: Question):
     try:
-        answer = collection.query(query_texts=[question.question], n_results=5)
+        answer = collection.query(query_texts=[question.question], n_results=5, where_document={"$contains": question.question[:2]})
         logging.info(answer)
         if not answer or not answer.get('metadatas') or len(answer.get('metadatas')) == 0:
-            return {
-                "error": "No answer found",
-            }
+            return [{
+                "question": "No question found.",
+                "answer": "No answer found.",
+            }]
         else:
             return answer.get('metadatas')[0]
     except Exception as e:
-        return {
-            "error": str(e),
-        }
+        return [{
+            "question": "No question found.",
+            "answer": "No answer found.",
+        }]
 
 
 if __name__ == "__main__":
