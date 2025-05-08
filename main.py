e22f354b0c1198bf5070417061140f7a015019be
le.toille <125640909+letoille@users.noreply.github.com>
2025-05-08 11:52:22 +0800

    1

--- main.py
+++ main.py
@@ -81,4 +81,4 @@ if __name__ == "__main__":
     else:
         logging.info("ChromaDB already loaded, skipping load.")
 
-    uvicorn.run(app, host="127.0.0.1", port=9527)
+    uvicorn.run(app, host="0.0.0.0", port=9527)
