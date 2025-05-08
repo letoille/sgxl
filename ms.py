35ecd6c72ae6b2d0bf97a0a513d159053dee7059
le.toille <125640909+letoille@users.noreply.github.com>
2025-05-08 15:37:30 +0800

    filter dup

--- ms.py
+++ ms.py
@@ -3,6 +3,7 @@ import json
 with open("data/ms.md", "r") as f:
     lines = f.readlines()
     qas = []
+    seen = set()  # 用于记录已处理的问答对
     for line in lines:
         line = line.strip()
         segs = line.split("-----")
@@ -11,8 +12,12 @@ with open("data/ms.md", "r") as f:
                 "question": "".join(question[1:]),
                 "answer": "".join(segs[1:]),
             }
-        qas.append(qa)
-        print(qa)
+        # 检查是否重复
+        if (qa["question"], qa["answer"]) not in seen:
+            qas.append(qa)
+            seen.add((qa["question"], qa["answer"]))
+            print(qa)
+
     with open("data/ms.json", "w") as f:
         json.dump(qas, f, ensure_ascii=False, indent=4)
         print("ms.json created.")
